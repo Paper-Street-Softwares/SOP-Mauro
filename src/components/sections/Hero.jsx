917c01d8e6baa45a3904ab2db77ce5abd4daed8a
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-15 16:27:23 -0300

    fix: Image heroMobile

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -161,6 +161,10 @@ export default function Hero({
                 <div className="flex justify-center w-full tablet1:w-[450px] desktop1:w-[42%] desktop2:w-[42.8%]">
                   <div className="relative flex justify-center w-full">
                     <picture>
+                      <source
+                        srcSet={content.texts.hero.heroDefaultImageMobile}
+                        media="(max-width: 424px)"
+                      />
                       <img
                         src="/lawHero.webp"
                         alt={content.texts.hero.alt}
